7852b4d9dc4628b168c9360b8bd3731dc0ae83dc
chavesjoabe <chavesjoabe1@gmail.com>
2022-03-07 19:18:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import Routes from './Routes.js';
+dotenv.config();
+
+class App {
+    server = express();
+    port = process.env.PORT || 8080;
+    databaseUrl = process.env.DATABASE_URL;
+    constructor() {
+        this.middlewares();
+    }
+
+    middlewares() {
+        this.server.use(express.json());
+        this.server.use(Routes);
+    }
+
+    createServer() {
+        this.server.listen(this.port, () => {
+            console.log('server is running in port', this.port);
+        });
+    }
+
+    createDatabaseConnection() {
+        console.log(process.env);
+        mongoose.connect(this.databaseUrl);
+        const db = mongoose.connection;
+        db.on('open', () => {
+            console.log('database connected');
+        });
+        db.on('error', () => {
+            console.log('error on database connection');
+        });
+    }
+}
+
+export default new App();
---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+class Routes {
+    routes = express.Router();
+    constructor() {
+        this.loadRoutes;
+    }
+
+    loadRoutes() {
+        this.routes.get('/all-chats', async (req, res) => {
+            const chatSchemasModel = mongoose.model('chatSchemas', {});
+
+            const chatSchemas = await chatSchemasModel.find();
+
+            return res.json(chatSchemas);
+        });
+    }
+}
+export default new Routes().routes;
